0137e09870d3eb9fa6e07d57c3e5e235c8cb84d1
Belbertn <belbert95@me.com>
2018-01-12 18:35:15 +0100

    jhj

---
+++ TowerGame/TowerGame/PhysicsActor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+
+namespace TowerGame
+{
+    class PhysicsActor : ICollideable
+    {
+        public Vector2 Position { get; set; }
+        public Rectangle Collider { get; set; }
+
+        public void Initialize(Vector2 postion, Rectangle rectangle)
+        {
+            Position = postion;
+            Collider = rectangle;
+        }
+    }
+}
